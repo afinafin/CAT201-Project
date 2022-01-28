45d66aaf1cfb41a4e36ac611529a911a5a3ea8ee
MalaysiaIronMan <94528592+MalaysiaIronMan@users.noreply.github.com>
2022-01-29 02:41:13 +0800

    Create LoseWeight.java

---
+++ LoseWeight.java
@@ -0,0 +1,20 @@
+package com.example.cat201try2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class LoseWeight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose_weight);
+        setTitle("Lose Weight");
+
+        //EditText UserAnswer = findViewById(R.id.UserTargetLoseWeight);
+        //int targetLoseWeight = Integer.parseInt(UserAnswer.getText().toString());
+
+    }
+}
