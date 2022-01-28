436f74dba0498825354f84e8076fc3687a8dece9
MalaysiaIronMan <94528592+MalaysiaIronMan@users.noreply.github.com>
2022-01-29 02:40:26 +0800

    Create GainWeight.java

---
+++ GainWeight.java
@@ -0,0 +1,23 @@
+package com.example.cat201try2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class GainWeight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gain_weight);
+        setTitle("Gain Weight");
+
+       // EditText UserAnswer = findViewById(R.id.UserTargetGainWeight);
+        //int targetGainWeight = Integer.parseInt(UserAnswer.getText().toString());
+
+
+
+
+    }
+}
