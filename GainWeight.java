59fd9235749992d4bf1273c149ddbbe25251b170
MalaysiaIronMan <94528592+MalaysiaIronMan@users.noreply.github.com>
2022-01-29 17:01:45 +0800

    Update GainWeight.java

--- GainWeight.java
+++ GainWeight.java
@@ -2,8 +2,14 @@ package com.example.cat201try2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GainWeight extends AppCompatActivity {
 
@@ -13,11 +19,48 @@ public class GainWeight extends AppCompatActivity {
         setContentView(R.layout.activity_gain_weight);
         setTitle("Gain Weight");
 
-       // EditText UserAnswer = findViewById(R.id.UserTargetGainWeight);
-        //int targetGainWeight = Integer.parseInt(UserAnswer.getText().toString());
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("UserPrefs", MODE_PRIVATE);
 
+        TextView currentweightShow = findViewById(R.id.viewCurrentWeight1);
+        Float currentWeight = sp.getFloat("WEIGHT", 0);
+        currentweightShow.setText(currentWeight + "KG");
+
+        TextView caloriesMaintain = findViewById(R.id.caloriesMaintainShow);
+        caloriesMaintain.setText(sp.getInt("MAINTAIN", 0) + " calories");
+
+
+
+        Button calculate = findViewById(R.id.calculateLoseWeight1);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText UserAnswer = findViewById(R.id.UserTargetGainWeight);
+                Float targetGainWeight = Float.parseFloat(UserAnswer.getText().toString());
+
+                if (targetGainWeight <= currentWeight) {
+                    Toast.makeText(GainWeight.this, "Target weight must greater than current weight", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    saveData(targetGainWeight);
+                    Intent intent = new Intent(GainWeight.this, GainWeightChoice.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    public void saveData(Float x){
+
+        SharedPreferences sp2 = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp2.edit();
+
+        editor.putFloat("GAIN",x);
+        editor.commit();
+    }
 
 
 
     }
-}
